6fd30094e6b30657f97f2d9ca324ab3d54f4aece
Benjamin Cho <bkcho1@asu.edu>
2023-11-29 22:29:23 -0700

    v1.2.2

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const express = require('express');
+const expressApp = express();
+
+expressApp.use(express.static("renderer"))
+
+expressApp.set('view engine', 'ejs');
+
+expressApp.get('/', (req, res) => {
+    res.render(path.join(__dirname, './renderer/index.ejs'));
+});
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
-        height: 700
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
 
-    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+    mainWindow.loadURL('http://localhost:3000');
     mainWindow.removeMenu();
 }
 
 app.whenReady().then(() => {
     createWindow();
 
+    const server = expressApp.listen(3000, () => {
+        console.log('Express server is running on http://localhost:3000');
+    });
+
+    app.on('before-quit', () => {
+        server.close();
+    });
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
-      })
+    })
 });
 
 app.on('window-all-closed', () => {
