8cd775e2485edeea525b610770ca9035acbefbd8
Benjamin Cho <bkcho1@asu.edu>
2023-11-30 22:13:36 -0700

    express app v1.0

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+
 const express = require('express');
 const expressApp = express();
-
-expressApp.use(express.static("renderer"))
+const custRouter = require('./routes/customers');
+const appmntRouter = require('./routes/appointments');
 
 expressApp.set('view engine', 'ejs');
 
-expressApp.get('/', (req, res) => {
-    res.render(path.join(__dirname, './renderer/index.ejs'));
-});
+expressApp.use(express.static("renderer"));
+expressApp.use('/customers', custRouter);
+expressApp.use('/appointments', appmntRouter);
+
+let mainWindow;
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 700,
         webPreferences: {
@@ -20,7 +24,7 @@ function createWindow() {
         }
     });
 
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3000/');
     mainWindow.removeMenu();
 }
 
@@ -30,18 +34,18 @@ app.whenReady().then(() => {
     const server = expressApp.listen(3000, () => {
         console.log('Express server is running on http://localhost:3000');
     });
-
-    app.on('before-quit', () => {
-        server.close();
-    });
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
 });
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
+});
+
+app.on('before-quit', () => {
+    server.close();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 });
--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,38 +1,3 @@
 const main_menu = document.getElementById("main-menu-container");
 const appmnt_page = document.getElementById("appmnt-page");
 const cust_page = document.getElementById("cust-page");
-
-/*
-* page{0 = main page, 1 = appointment page, 2 = customer page}
-*/
-let page = 0;
-
-function render_appmnt_page(){
-    page = 1;
-    main_menu.style.display = 'none';
-    appmnt_page.style.display = 'block';
-}
-
-function render_cust_page() {
-    page = 2;
-    main_menu.style.display = 'none';
-    cust_page.style.display = 'block';
-}
-
-function return_to_main() {
-    main_menu.style.display = 'block';
-    switch(page){
-        case 1:
-            page = 0;
-            appmnt_page.style.display = 'none';
-            break;
-        case 2:
-            page = 0;
-            cust_page.style.display = 'none';
-            break;
-    }
-}
-
-function add_customer(){
-    
-}
